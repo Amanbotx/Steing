73e7f021a17664c9098ae7d0dd03a89f341b84d0
Amanbotx <159861177+Amanbotx@users.noreply.github.com>
2024-03-13 06:16:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,4 +12,4 @@ BOT_TOKEN = getenv("BOT_TOKEN", "6883904115:AAFzjOFjf30iab7YEh5dMQ5Y1D0V-6bBQ74"
 MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://ranjuvishwakarma50:aman@cluster0.nyw39zi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
 OWNER_ID = int(getenv("OWNER_ID", "5977931010"))
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Developerr_Bots_Support")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Amanchatgroup1")
