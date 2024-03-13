180895b54b9912e7004488b2219b025bcc03cfb9
Amanbotx <159861177+Amanbotx@users.noreply.github.com>
2024-03-13 06:15:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,8 +8,8 @@ load_dotenv()
 API_ID = int(getenv("API_ID","29219170"))
 API_HASH = getenv("API_HASH", "12d6648ede66e1ef31d9c455317ee09d")
 
-BOT_TOKEN = getenv("BOT_TOKEN", "6282594199:AAFXICV7X6uAOH2nELP3IruuU5o5TFylcBM")
-MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://Sohanrazz:Sohanrazz@cluster0.wbcmn82.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+BOT_TOKEN = getenv("BOT_TOKEN", "6883904115:AAFzjOFjf30iab7YEh5dMQ5Y1D0V-6bBQ74")
+MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://ranjuvishwakarma50:aman@cluster0.nyw39zi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
-OWNER_ID = int(getenv("OWNER_ID", "5897793065"))
+OWNER_ID = int(getenv("OWNER_ID", "5977931010"))
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Developerr_Bots_Support")
